8767714250ae6dbc6432f7fb2b9c5727a20b8a48
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-06-11 19:33:45 -0300

    Install zsh dracula theme

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ echo 'installing zsh'
 sudo apt install zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+# https://draculatheme.com/zsh/
+echo 'installing zsh dracula theme'
+git clone https://github.com/dracula/zsh.git ~/.dracula
+cp .dracula/dracula.zsh-theme .oh-my-zsh/themes/
+cp -r .dracula/lib/ .oh-my-zsh/themes/
+sed -i 's/ZSH_THEME=.*/ZSH_THEME="dracula"/g' ~/.zshrc
