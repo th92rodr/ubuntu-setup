fdbdcd307783c7b61a4527505461c6aee9129229
th92rodr <45070690+th92rodr@users.noreply.github.com>
2025-08-25 19:17:47 -0300

    Create a main function and install basic packages

--- setup.sh
+++ setup.sh
@@ -2,36 +2,14 @@
 
 source ./func.sh
 
-options=("Install and Configure" "Only Configure" "Ignore")
+main () {
+  sudo apt update && sudo apt upgrade -y
 
-echo -e "\e[34mGIT\e[0m"
-select do_git in "${options[@]}"
-do
-  case $do_git in
-    ${options[0]}|${options[1]})
-      echo -e "\e[32m$do_git\e[0m"
-      fn_git $REPLY
-      break
-      ;;
-    ${options[2]})
-      break
-      ;;
-    *)
-      echo -e "\e[31mInvalid option $REPLY\e[0m"
-      ;;
-  esac
-done
+  basic_packages=(curl wget unzip make gnome-tweaks gcc g++ build-essential tree)
 
-exit
+  for package in "${basic_packages[@]}"; do
+    safe_install "$package"
+  done
+}
 
-############
-
-if [[ $is_git = y ]] ; then
-  if [[ $is_git_ok = 1 ]] ; then
-    echo -e "\e[34m \nGIT done \e[0m"
-  else
-    echo -e "\e[34m \nGIT error \e[0m"
-  fi
-fi
-
-echo -e "\e[32m \n All set \e[0m"
+main "$@"
