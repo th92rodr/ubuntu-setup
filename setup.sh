8c49bbf8b934c120cd47fed142f879935172bd3a
th92rodr <45070690+th92rodr@users.noreply.github.com>
2026-02-15 17:10:56 -0300

    Add error handling

--- setup.sh
+++ setup.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 source ./func.sh
+source ./utils.sh
+
+set -o errexit -o errtrace -o nounset -o pipefail
+trap error_handler ERR
 
 main () {
   check_os
 
   sudo apt update && sudo apt upgrade -y
 
-  basic_packages=(curl wget unzip make gnome-tweaks gcc g++ build-essential)
+  local -r basic_packages=(curl wget unzip make gnome-tweaks gcc g++ build-essential)
 
   for package in "${basic_packages[@]}"; do
     safe_install "$package"
   done
 
-  additional_packages=(tree fzf peco)
+  local -r additional_packages=(tree fzf peco)
 
   for package in "${additional_packages[@]}"; do
     safe_install "$package"
   done
 
-  packages=(git brave zsh homebrew vscode docker nvm node yarn tig pyenv bat fd
-          protoc kubernetes gcloud golang postman java)
+  local -r packages=(git brave zsh homebrew vscode docker nvm node yarn tig pyenv bat fd
+              protoc kubernetes gcloud golang postman java)
 
   for package in "${packages[@]}"; do
     "install_$package"
---
+++ utils.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+error_handler () {
+  local exit_code=$?
+
+  log error "Error: command '${BASH_COMMAND}' failed with exit code ${exit_code}"
+  log error "Location: ${BASH_SOURCE[1]}:${BASH_LINENO[0]} (function: ${FUNCNAME[1]})"
+  log error "Stack trace:"
+
+  for i in "${!FUNCNAME[@]}"; do
+    if [ "$i" -gt 0 ]; then
+      log error "  at ${FUNCNAME[$i]}() in ${BASH_SOURCE[$i]}:${BASH_LINENO[$((i-1))]}"
+    fi
+  done
+
+  exit 1
+}
+
+soft_fail () {
+  if ! "$@"; then
+    log warn "Error: $*"
+  fi
+}
